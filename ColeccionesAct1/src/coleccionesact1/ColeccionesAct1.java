3ded2d817d634cd77ab1e61f9a80682c0211626d
Joaquin-CasVar <joaquincastellanovarela14@gmail.com>
2023-07-20 20:43:18 -0300

    Añadida Act2, Eliminar una raza y mostrar lista actualizada

--- ColeccionesAct1/src/coleccionesact1/ColeccionesAct1.java
+++ ColeccionesAct1/src/coleccionesact1/ColeccionesAct1.java
@@ -3,6 +3,7 @@ package coleccionesact1;
 // @author Haze
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class ColeccionesAct1 {
@@ -11,12 +12,29 @@ public class ColeccionesAct1 {
         ArrayList<String> perros = new ArrayList();
         Scanner input = new Scanner(System.in, "ISO-8859-1").useDelimiter("\n");
         String res = "S";
+        boolean flag = true;
         
         while (!res.equalsIgnoreCase("N")) {
             System.out.println("Ingrese una raza de perro");
             perros.add(input.next());
             System.out.println("Quiere guardar otro perro.(S/N)");
             res = (input.next());
+        }
+        System.out.println("Lista de perros cargada:");
+        perros.forEach(aux -> System.out.println(aux));
+        
+        System.out.println("Ingrese una raza de perro a eliminar");
+        res = (input.next());
+        
+        Iterator<String> it = perros.iterator();
+        while (it.hasNext()) {
+            if (it.next().equalsIgnoreCase(res)) {
+                it.remove();
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("No se encontro la raza ingresada");
         }
         System.out.println("Lista de perros cargada:");
         perros.forEach(aux -> System.out.println(aux));
